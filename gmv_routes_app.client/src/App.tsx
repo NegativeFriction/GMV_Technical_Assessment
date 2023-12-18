9309ea6bb4fa7b0767f51e1f4278f62212223f69
Chris <chris.chrispete@gmail.com>
2023-12-18 11:47:03 -0800

    Correct issues with fonts for the main logo

--- gmv_routes_app.client/src/App.tsx
+++ gmv_routes_app.client/src/App.tsx
@@ -5,17 +5,23 @@ import {
 import { useState } from "react";
 import HomePage from "./Pages/Home_Page/HomePage";
 import ArrivalsPage from "./Pages/Arrivals_Page/ArrivalsPage";
+import Header from "./Common/Header/Header";
+import "./App.css";
 
 function App() {
   const [selectedRouteContextValue, setSelectedRouteContextValue] =
     useState<SelectedRouteContextType>(null);
   return (
     <SelectedRouteContext.Provider value={selectedRouteContextValue}>
+      <Header setSelectedRouteContextValue={setSelectedRouteContextValue} />
       {!selectedRouteContextValue && (
         <HomePage setSelectedRouteContextValue={setSelectedRouteContextValue} />
       )}
       {selectedRouteContextValue && (
-        <ArrivalsPage {...selectedRouteContextValue} />
+        <ArrivalsPage
+          {...selectedRouteContextValue}
+          setSelectedRouteContextValue={setSelectedRouteContextValue}
+        />
       )}
     </SelectedRouteContext.Provider>
   );
